e0427b464a065372a72581e0afdbf23b3ab8890a
hackrsackr <rob.knighton10@gmail.com>
2022-06-23 11:26:12 -0400

    y

---
+++ brewblox_spunders/include/Spunder/spunder_config.hpp
@@ -0,0 +1,85 @@
+#include "secrets.h"
+
+// Number of sensor solenoid pairs
+#define _NUMBER_OF_SPUNDERS 4
+
+// Relay logic
+#define _RELAY_OPEN HIGH
+
+// Reference voltage
+#define _VESP 4.47 // actual voltage of esp 5v channel
+#define _PUSB 5.00 // actual voltage the pi usb supplies
+
+// WiFi credentials
+#define _SSID SECRET_SSID
+#define _PASS SECRET_PASS
+
+// MQTT setup
+#define _MQTTHOST "192.168.1.2"
+#define _MQTTPORT 1883
+#define _CLIENTID "spunders"
+#define _SUBTOPIC "brewcast/history/spark-two"
+#define _PUBTOPIC "brewcast/history/spunders"
+
+// Name of each spunder object
+#define _NAME1 "spunder1"
+#define _NAME2 "spunder2"
+#define _NAME3 "spunder3"
+#define _NAME4 "spunder4"
+
+std::array<const String, _NUMBER_OF_SPUNDERS>
+SPUNDER_NAMES = { _NAME1, _NAME2, _NAME3, _NAME4 };
+
+// Brewblox names of temps to request for each spunder mqtt_temp_field
+#define _TEMP1 "TEMP_orangeBeer"
+#define _TEMP2 "TEMP_orangeFridge"
+#define _TEMP3 "TEMP_blueBeer"
+#define _TEMP4 "TEMP_blueFridge"
+
+std::array<const String, _NUMBER_OF_SPUNDERS>
+MQTT_FIELDS = { _TEMP1, _TEMP2, _TEMP3, _TEMP4 };
+
+// Desired vols of CO2 for each spunder
+#define _VOLS1 2.0
+#define _VOLS2 2.2
+#define _VOLS3 3.0
+#define _VOLS4 4.0
+
+std::array<const float, _NUMBER_OF_SPUNDERS>
+DESIRED_VOLS = { _VOLS1, _VOLS2, _VOLS3, _VOLS4 };
+
+// Max units of each pressure sensor in psi
+#define _UMAX1 30.0
+#define _UMAX2 60.0
+#define _UMAX3 60.0
+#define _UMAX4 60.0
+
+std::array<const float, _NUMBER_OF_SPUNDERS>
+UNIT_MAXS = { _UMAX1, _UMAX2, _UMAX3, _UMAX4 };
+
+// Pin of the relay of each spunder
+#define _RPIN1 14
+#define _RPIN2 27
+#define _RPIN3 16
+#define _RPIN4 17
+
+std::array<const uint8_t, _NUMBER_OF_SPUNDERS>
+RELAY_PINS = { _RPIN1, _RPIN2, _RPIN3, _RPIN4 };
+
+// Voltage at zero pressure using esp vcc
+#define _ESPV1 .54
+#define _ESPV2 .43
+#define _ESPV3 .43
+#define _ESPV4 .42
+
+std::array<const float, _NUMBER_OF_SPUNDERS>
+ESP_VOLTS ={ _ESPV1, _ESPV2, _ESPV3, _ESPV4 };
+
+// Voltage at zero pressure using pi usb as 5v source
+#define _PUSB1 .54
+#define _PUSB2 .48
+#define _PUSB3 .47
+#define _PUSB4 .46
+
+std::array<const float, _NUMBER_OF_SPUNDERS>
+USB_VOLTS = { _PUSB1, _PUSB2, _PUSB3, _PUSB4 };
---
+++ brewblox_spunders/src/spund_server.cpp
@@ -0,0 +1,210 @@
+#include <iostream>
+#include <WiFi.h>
+#include <Wire.h>
+#include <ArduinoJson.h>
+#include <Adafruit_ADS1X15.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <EspMQTTClient.h>
+
+#include "Spunder/spunder.hpp"
+#include "Spunder/spunder_config.hpp"
+
+// Server
+AsyncWebServer server(80);
+String inputMessage1 = String(DESIRED_VOLS[0]);
+String inputMessage2 = String(DESIRED_VOLS[1]);
+String inputMessage3 = String(DESIRED_VOLS[2]);
+String inputMessage4 = String(DESIRED_VOLS[3]);
+const char *PARAM_INPUT_1 = "input1";
+const char *PARAM_INPUT_2 = "input2";
+const char *PARAM_INPUT_3 = "input3";
+const char *PARAM_INPUT_4 = "input4";
+
+void notFound(AsyncWebServerRequest *request) { request->send(404, "text/plain", "Not found"); }
+
+String processor(const String &var)
+{
+  // std::cout << var << std::endl;
+  if (var == "SETPOINT1") { return inputMessage1; }
+  if (var == "SETPOINT2") { return inputMessage2; }
+  if (var == "SETPOINT3") { return inputMessage3; }
+  if (var == "SETPOINT4") { return inputMessage4; }
+  return String();
+}
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>Spunder Setpoints</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    body {
+    padding: 25px;
+    background-color: black;
+    color: grey;
+    font-size: 25px;
+    }
+  </style></head>
+  <body>
+  <p>Spunder Setpoints vols[co2]</p>
+  <form action="/get">
+    sepoint-1: <input type="text" name="input1">
+    <input type="submit" value="Submit">
+  </form><br>
+  <form action="/get">
+    sepoint-2: <input type="text" name="input2">
+    <input type="submit" value="Submit">
+  </form><br>
+  <form action="/get">
+    sepoint-3: <input type="text" name="input3">
+    <input type="submit" value="Submit">
+  </form><br>
+  <form action="/get">
+    sepoint-4: <input type="text" name="input4">
+    <input type="submit" value="Submit">
+  </form>
+</body></html>)rawliteral";
+
+std::array<Spunder, _NUMBER_OF_SPUNDERS> spund_arr;
+EspMQTTClient client(_SSID, _PASS, _MQTTHOST, _CLIENTID, _MQTTPORT);
+StaticJsonDocument<4096> input;
+void onConnectionEstablished(void);
+void publishData(void);
+
+void setup()
+{
+  Serial.begin(115200);
+  ads.begin(0x48);
+  ads.setGain(GAIN_TWOTHIRDS);
+
+  client.setMaxPacketSize(4096);
+  client.enableOTA();
+  // client.enableDebuggingMessages();
+
+  // WiFi
+  WiFi.disconnect(true);
+  delay(1000);
+  WiFi.begin(_SSID, _PASS);
+  uint8_t failed_connections = 0;
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("connecting..");
+    failed_connections++;
+    if (failed_connections > 20)
+    {
+      Serial.println("restarting..");
+      ESP.restart();
+    }
+  }
+  Serial.print("Connected to ");
+  Serial.println(WiFi.localIP());
+
+  // Spunder setup
+  for (uint8_t spunder = 0; spunder < _NUMBER_OF_SPUNDERS; spunder++)
+  {
+    spund_arr[spunder].name = SPUNDER_NAMES[spunder];
+    spund_arr[spunder].mqtt_field = MQTT_FIELDS[spunder];
+    spund_arr[spunder].vols_setpoint = DESIRED_VOLS[spunder];
+    spund_arr[spunder].unit_max = UNIT_MAXS[spunder];
+    spund_arr[spunder].relay_pin = RELAY_PINS[spunder];
+    spund_arr[spunder].offset_volts = ESP_VOLTS[spunder];
+
+    spund_arr[spunder].esp_vusb = _VESP;
+    spund_arr[spunder].stored_time = millis();
+    spund_arr[spunder].ads_channel = spunder;
+
+    pinMode(spund_arr[spunder].relay_pin, OUTPUT);
+    digitalWrite(spund_arr[spunder].relay_pin, !_RELAY_OPEN);
+  }
+
+  // Send web page with input fields to client
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send_P(200, "text/html", index_html, processor); });
+  server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+    String inputMessage;
+    String inputParam;
+    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+    if (request->hasParam(PARAM_INPUT_1)) {
+      inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
+      spund_arr[0].vols_setpoint = inputMessage1.toFloat();
+      inputMessage = inputMessage1;
+      inputParam = PARAM_INPUT_1;
+    }
+    // GET input2 value on <ESP_IP>/get?input2=<inputMessage>
+    else if (request->hasParam(PARAM_INPUT_2)) {
+      inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
+      spund_arr[1].vols_setpoint = inputMessage2.toFloat();
+      inputMessage = inputMessage2;
+      inputParam = PARAM_INPUT_2;
+    }
+    // GET input3 value on <ESP_IP>/get?input3=<inputMessage>
+    else if (request->hasParam(PARAM_INPUT_3)) {
+      inputMessage3 = request->getParam(PARAM_INPUT_3)->value();
+      spund_arr[2].vols_setpoint = inputMessage3.toFloat();
+      inputMessage = inputMessage3;
+      inputParam = PARAM_INPUT_3;
+    }
+    else if (request->hasParam(PARAM_INPUT_4)) {
+      inputMessage4 = request->getParam(PARAM_INPUT_4)->value();
+      spund_arr[3].vols_setpoint = inputMessage4.toFloat();
+      inputMessage = inputMessage4;
+      inputParam = PARAM_INPUT_4;
+    }
+    std::cout << inputParam << ": " << inputMessage << std::endl;
+    //request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
+    //                                 + inputParam + ") with value: " + inputMessage +
+    //                                "<br><a href=\"/\">Return to Home Page</a>"); });
+    request->send(200, "text/html", inputMessage + "<br><a href=\"/\">Back</a>"); });
+  server.onNotFound(notFound);
+  server.begin();
+}
+
+void loop() { client.loop(); }
+
+void onConnectionEstablished()
+{
+  client.subscribe(_SUBTOPIC, [](const String &payload)
+  {
+    //std::cout << payload << std::endl;
+    deserializeJson(input, payload);
+    for (uint8_t spunder = 0; spunder < _NUMBER_OF_SPUNDERS; spunder++)
+    {
+      spund_arr[spunder].tempC = input["data"][spund_arr[spunder].mqtt_field]["value[degC]"];
+      //std::cout << spund_arr[spunder].tempC << std::endl;
+    }
+    publishData(); 
+  });
+}
+
+void publishData()
+{
+  if (client.isConnected())
+  {
+    //DynamicJsonDocument message(768);
+    StaticJsonDocument<768> message;
+    message["key"] = _CLIENTID;
+
+    // Read each spunder in the array of spunders
+    for (uint8_t spunder = 0; spunder < _NUMBER_OF_SPUNDERS; spunder++)
+    {
+      if (!spund_arr[spunder].tempC) 
+      { Serial.println(" no temp reading"); 
+        continue; 
+      } else { spund_arr[spunder].spunder_run(); }
+
+      message["data"][spund_arr[spunder].name]["volts"] = spund_arr[spunder].volts;
+      message["data"][spund_arr[spunder].name]["tempC"] = spund_arr[spunder].tempC;
+      message["data"][spund_arr[spunder].name]["psi_setpoint"] = spund_arr[spunder].psi_setpoint;
+      message["data"][spund_arr[spunder].name]["psi"] = spund_arr[spunder].psi_value;
+      message["data"][spund_arr[spunder].name]["vols_target"] = spund_arr[spunder].vols_setpoint;
+      message["data"][spund_arr[spunder].name]["volumes[co2]"] = spund_arr[spunder].vols_value;
+      message["data"][spund_arr[spunder].name]["since_vent"] = spund_arr[spunder].time_since_vent;
+    }
+    serializeJsonPretty(message, Serial);
+    client.publish(_PUBTOPIC, message.as<String>());
+    delay(5000);
+  }
+}
+
