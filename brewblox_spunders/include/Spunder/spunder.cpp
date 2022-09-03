8e8f85ae7ff890789bcd486595ddf4344ca43515
hackrsackr <rob.knighton10@gmail.com>
2022-09-02 21:06:57 -0400

    del comments

--- brewblox_spunders/include/Spunder/spunder.cpp
+++ brewblox_spunders/include/Spunder/spunder.cpp
@@ -7,15 +7,10 @@
 #include "Spunder/spunder_config.hpp"
 
 // From spunder_config.h
-// Create a MQTT client
 EspMQTTClient client(_SSID, _PASS, _MQTTHOST, _CLIENTID, _MQTTPORT);
-
-// Create array of Spunders
+StaticJsonDocument<4096> input;
 std::array<Spunder, _NUMBER_OF_SPUNDERS> spund_arr;
 
-DynamicJsonDocument input(4096);
-
-// Client run function
 void onConnectionEstablished(void);
 void publishData(void);
 
@@ -28,7 +23,6 @@ void setup()
     client.enableHTTPWebUpdater();
     client.setMaxPacketSize(4096);
     client.enableOTA();
-    //client.enableDebuggingMessages();
 
     // WiFi
     WiFi.disconnect(true);
@@ -39,7 +33,7 @@ void setup()
     {
         delay(500);
         Serial.println("connecting..");
-        failed_connections ++;
+        failed_connections++;
         if (failed_connections > 20)
         {
             Serial.println("restarting..");
@@ -52,12 +46,12 @@ void setup()
     // Spunder setup
     for (uint8_t spunder = 0; spunder < _NUMBER_OF_SPUNDERS; spunder++)
     {
-        spund_arr[spunder].name          = SPUNDER_NAMES[spunder];
-        spund_arr[spunder].mqtt_field    = MQTT_FIELDS[spunder];
+        spund_arr[spunder].name = SPUNDER_NAMES[spunder];
+        spund_arr[spunder].mqtt_field = MQTT_FIELDS[spunder];
         spund_arr[spunder].vols_setpoint = DESIRED_VOLS[spunder];
-        spund_arr[spunder].unit_max      = UNIT_MAXS[spunder];
-        spund_arr[spunder].relay_pin     = RELAY_PINS[spunder];
-        spund_arr[spunder].offset_volts  = ESP_VOLTS[spunder];
+        spund_arr[spunder].unit_max = UNIT_MAXS[spunder];
+        spund_arr[spunder].relay_pin = RELAY_PINS[spunder];
+        spund_arr[spunder].offset_volts = ESP_VOLTS[spunder];
 
         spund_arr[spunder].esp_vusb = _VESP;
         spund_arr[spunder].stored_time = millis();
@@ -71,46 +65,44 @@ void setup()
 void onConnectionEstablished()
 {
     client.subscribe(_SUBTOPIC, [](const String &payload)
-    {
+                     {
         Serial.println(payload);
         deserializeJson(input, payload);
         for (uint8_t spunder = 0; spunder < _NUMBER_OF_SPUNDERS; spunder++)
         {
           spund_arr[spunder].tempC = input["data"][spund_arr[spunder].mqtt_field]["value[degC]"];
-          //Serial.println(spund_arr[spunder].tempC);
         }
-        publishData();
-    });
+        publishData(); });
 }
-
 
 void publishData()
 {
     if (client.isConnected())
     {
-
-        DynamicJsonDocument message(768);
+        StaticJsonDocument<768> message;
         message["key"] = _CLIENTID;
 
         // Read each spunder in the array of spunders
         for (uint8_t spunder = 0; spunder < _NUMBER_OF_SPUNDERS; spunder++)
         {
-            if (!spund_arr[spunder].tempC) {
+            if (!spund_arr[spunder].tempC)
+            {
                 Serial.println(" no temp reading");
                 continue;
-
-            } else {
+            }
+            else
+            {
                 spund_arr[spunder].spunder_run();
             }
 
             // Populate data message to publish to brewblox
-            message["data"][spund_arr[spunder].name]["volts"]        = spund_arr[spunder].volts;
-            message["data"][spund_arr[spunder].name]["tempC"]        = spund_arr[spunder].tempC;
+            message["data"][spund_arr[spunder].name]["volts"] = spund_arr[spunder].volts;
+            message["data"][spund_arr[spunder].name]["tempC"] = spund_arr[spunder].tempC;
             message["data"][spund_arr[spunder].name]["psi_setpoint"] = spund_arr[spunder].psi_setpoint;
-            message["data"][spund_arr[spunder].name]["psi"]          = spund_arr[spunder].psi_value;
-            message["data"][spund_arr[spunder].name]["vols_target"]  = spund_arr[spunder].vols_setpoint;
+            message["data"][spund_arr[spunder].name]["psi"] = spund_arr[spunder].psi_value;
+            message["data"][spund_arr[spunder].name]["vols_target"] = spund_arr[spunder].vols_setpoint;
             message["data"][spund_arr[spunder].name]["volumes[co2]"] = spund_arr[spunder].vols_value;
-            message["data"][spund_arr[spunder].name]["since_vent"]   = spund_arr[spunder].time_since_vent;
+            message["data"][spund_arr[spunder].name]["since_vent"] = spund_arr[spunder].time_since_vent;
         }
 
         serializeJsonPretty(message, Serial);
--- brewblox_spunders/src/spund_server.cpp
+++ brewblox_spunders/src/spund_server.cpp
@@ -54,23 +54,35 @@ const char index_html[] PROGMEM = R"rawliteral(
   </form>
 </body></html>)rawliteral";
 
-void notFound(AsyncWebServerRequest *request) { 
-  request->send(404, "text/plain", "Not found"); 
+void notFound(AsyncWebServerRequest *request)
+{
+    request->send(404, "text/plain", "Not found");
 }
 
-String processor(const String &var) {
-  // Serial.println(var);
-  if (var == "SETPOINT1") { return inputMessage1; }
-  if (var == "SETPOINT2") { return inputMessage2; }
-  if (var == "SETPOINT3") { return inputMessage3; }
-  if (var == "SETPOINT4") { return inputMessage4; }
-  return String();
+String processor(const String &var)
+{
+    if (var == "SETPOINT1")
+    {
+        return inputMessage1;
+    }
+    if (var == "SETPOINT2")
+    {
+        return inputMessage2;
+    }
+    if (var == "SETPOINT3")
+    {
+        return inputMessage3;
+    }
+    if (var == "SETPOINT4")
+    {
+        return inputMessage4;
+    }
+    return String();
 }
 
 std::array<Spunder, _NUMBER_OF_SPUNDERS> spund_arr;
 
 EspMQTTClient client(_SSID, _PASS, _MQTTHOST, _CLIENTID, _MQTTPORT);
-//DynamicJsonDocument input(4096);
 StaticJsonDocument<4096> input;
 
 void onConnectionEstablished(void);
@@ -78,98 +90,101 @@ void publishData(void);
 
 void setup()
 {
-  Serial.begin(115200);
-  ads.begin(0x48);
-  ads.setGain(GAIN_TWOTHIRDS);
+    Serial.begin(115200);
+    ads.begin(0x48);
+    ads.setGain(GAIN_TWOTHIRDS);
 
-  client.setMaxPacketSize(4096);
-  client.enableOTA();
-  // client.enableDebuggingMessages();
+    client.setMaxPacketSize(4096);
+    client.enableOTA();
 
-  // WiFi
-  WiFi.disconnect(true);
-  delay(1000);
-  WiFi.begin(_SSID, _PASS);
-  uint8_t failed_connections = 0;
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.println("connecting..");
-    failed_connections++;
-    if (failed_connections > 20)
+    // WiFi
+    WiFi.disconnect(true);
+    delay(1000);
+    WiFi.begin(_SSID, _PASS);
+    uint8_t failed_connections = 0;
+    while (WiFi.status() != WL_CONNECTED)
     {
-      Serial.println("restarting..");
-      ESP.restart();
+        delay(500);
+        Serial.println("connecting..");
+        failed_connections++;
+        if (failed_connections > 20)
+        {
+            Serial.println("restarting..");
+            ESP.restart();
+        }
     }
-  }
-  Serial.print("Connected to ");
-  Serial.println(WiFi.localIP());
+    Serial.print("Connected to ");
+    Serial.println(WiFi.localIP());
 
-  // Spunder setup
-  for (uint8_t spunder = 0; spunder < _NUMBER_OF_SPUNDERS; spunder++)
-  {
-    spund_arr[spunder].name = SPUNDER_NAMES[spunder];
-    spund_arr[spunder].mqtt_field = MQTT_FIELDS[spunder];
-    spund_arr[spunder].vols_setpoint = DESIRED_VOLS[spunder];
-    spund_arr[spunder].unit_max = UNIT_MAXS[spunder];
-    spund_arr[spunder].relay_pin = RELAY_PINS[spunder];
-    spund_arr[spunder].offset_volts = ESP_VOLTS[spunder];
+    // Spunder setup
+    for (uint8_t spunder = 0; spunder < _NUMBER_OF_SPUNDERS; spunder++)
+    {
+        spund_arr[spunder].name = SPUNDER_NAMES[spunder];
+        spund_arr[spunder].mqtt_field = MQTT_FIELDS[spunder];
+        spund_arr[spunder].vols_setpoint = DESIRED_VOLS[spunder];
+        spund_arr[spunder].unit_max = UNIT_MAXS[spunder];
+        spund_arr[spunder].relay_pin = RELAY_PINS[spunder];
+        spund_arr[spunder].offset_volts = ESP_VOLTS[spunder];
 
-    spund_arr[spunder].esp_vusb = _VESP;
-    spund_arr[spunder].stored_time = millis();
-    spund_arr[spunder].ads_channel = spunder;
+        spund_arr[spunder].esp_vusb = _VESP;
+        spund_arr[spunder].stored_time = millis();
+        spund_arr[spunder].ads_channel = spunder;
 
-    pinMode(spund_arr[spunder].relay_pin, OUTPUT);
-    digitalWrite(spund_arr[spunder].relay_pin, !_RELAY_OPEN);
-  }
+        pinMode(spund_arr[spunder].relay_pin, OUTPUT);
+        digitalWrite(spund_arr[spunder].relay_pin, !_RELAY_OPEN);
+    }
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send_P(200, "text/html", index_html, processor); });
-  server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send_P(200, "text/html", index_html, processor); });
+    server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
     String inputMessage;
     String inputParam;
-    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+
     if (request->hasParam(PARAM_INPUT_1)) {
+      // GET input1 values on <ESP_IP>/get?input1=<inputMessage>
       inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
       spund_arr[0].vols_setpoint = inputMessage1.toFloat();
       inputMessage = inputMessage1;
       inputParam = PARAM_INPUT_1;
     }
-    // GET input2 value on <ESP_IP>/get?input2=<inputMessage>
     else if (request->hasParam(PARAM_INPUT_2)) {
+      // GET input2 value on <ESP_IP>/get?input2=<inputMessage>
       inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
       spund_arr[1].vols_setpoint = inputMessage2.toFloat();
       inputMessage = inputMessage2;
       inputParam = PARAM_INPUT_2;
     }
-    // GET input3 value on <ESP_IP>/get?input3=<inputMessage>
     else if (request->hasParam(PARAM_INPUT_3)) {
+      // GET input3 value on <ESP_IP>/get?input3=<inputMessage>
       inputMessage3 = request->getParam(PARAM_INPUT_3)->value();
       spund_arr[2].vols_setpoint = inputMessage3.toFloat();
       inputMessage = inputMessage3;
       inputParam = PARAM_INPUT_3;
     }
     else if (request->hasParam(PARAM_INPUT_4)) {
+      // GET input4 value on <ESP_IP>/get?input4=<inputMessage>
       inputMessage4 = request->getParam(PARAM_INPUT_4)->value();
       spund_arr[3].vols_setpoint = inputMessage4.toFloat();
       inputMessage = inputMessage4;
       inputParam = PARAM_INPUT_4;
     }
+
     Serial.println(inputMessage);
-    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
+
+    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field ("
                                      + inputParam + ") with value: " + inputMessage +
                                      "<br><a href=\"/\">Return to Home Page</a>"); });
-  server.onNotFound(notFound);
-  server.begin();
+    server.onNotFound(notFound);
+    server.begin();
 }
 
 void loop() { client.loop(); }
 
 void onConnectionEstablished()
 {
-  client.subscribe(_SUBTOPIC, [](const String &payload)
-  {
+    client.subscribe(_SUBTOPIC, [](const String &payload)
+                     {
     Serial.println(payload);
     deserializeJson(input, payload);
     for (uint8_t spunder = 0; spunder < _NUMBER_OF_SPUNDERS; spunder++)
@@ -182,32 +197,33 @@ void onConnectionEstablished()
 
 void publishData()
 {
-  if (client.isConnected())
-  {
-    StaticJsonDocument<768> message;
-    //DynamicJsonDocument message(768);
-    message["key"] = _CLIENTID;
-
-    // Read each spunder in the array of spunders
-    for (uint8_t spunder = 0; spunder < _NUMBER_OF_SPUNDERS; spunder++)
+    if (client.isConnected())
     {
-      if (!spund_arr[spunder].tempC)
-      {
-        Serial.println(" no temp reading");
-        continue;
-      } else { spund_arr[spunder].spunder_run(); }
+        StaticJsonDocument<768> message;
+        message["key"] = _CLIENTID;
 
-      message["data"][spund_arr[spunder].name]["volts"] = spund_arr[spunder].volts;
-      message["data"][spund_arr[spunder].name]["tempC"] = spund_arr[spunder].tempC;
-      message["data"][spund_arr[spunder].name]["psi_setpoint"] = spund_arr[spunder].psi_setpoint;
-      message["data"][spund_arr[spunder].name]["psi"] = spund_arr[spunder].psi_value;
-      message["data"][spund_arr[spunder].name]["vols_target"] = spund_arr[spunder].vols_setpoint;
-      message["data"][spund_arr[spunder].name]["volumes[co2]"] = spund_arr[spunder].vols_value;
-      message["data"][spund_arr[spunder].name]["since_vent"] = spund_arr[spunder].time_since_vent;
+        for (uint8_t spunder = 0; spunder < _NUMBER_OF_SPUNDERS; spunder++)
+        {
+            if (!spund_arr[spunder].tempC)
+            {
+                Serial.println(" no temp reading");
+                continue;
+            }
+            else
+            {
+                spund_arr[spunder].spunder_run();
+            }
+
+            message["data"][spund_arr[spunder].name]["volts"] = spund_arr[spunder].volts;
+            message["data"][spund_arr[spunder].name]["tempC"] = spund_arr[spunder].tempC;
+            message["data"][spund_arr[spunder].name]["psi_setpoint"] = spund_arr[spunder].psi_setpoint;
+            message["data"][spund_arr[spunder].name]["psi"] = spund_arr[spunder].psi_value;
+            message["data"][spund_arr[spunder].name]["vols_target"] = spund_arr[spunder].vols_setpoint;
+            message["data"][spund_arr[spunder].name]["volumes[co2]"] = spund_arr[spunder].vols_value;
+            message["data"][spund_arr[spunder].name]["since_vent"] = spund_arr[spunder].time_since_vent;
+        }
+        serializeJsonPretty(message, Serial);
+        client.publish(_PUBTOPIC, message.as<String>());
+        delay(5000);
     }
-    serializeJsonPretty(message, Serial);
-    client.publish(_PUBTOPIC, message.as<String>());
-    delay(5000);
-  }
 }
-
